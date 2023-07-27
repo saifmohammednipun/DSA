505494a5896e28fe0920a7b6f1594bec993f522a
saifmohammednipun <saifmohammednipun@gmail.com>
2023-07-27 22:29:31 +0600

    Update

---
+++ Sorting/Bubble_Sort/1.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+#define SIZE 6
+
+int main()
+{
+    int myArray[SIZE] = { 50, 12, 65, -5, 10, -8 };
+    int temp = 0; 
+
+    for(int i=0; i<SIZE; i++)
+    {
+        for(int j=0; j<SIZE-i; j++)
+        {
+            if(myArray[j] > myArray[j+1])
+            {
+                temp = myArray[j];
+                myArray[j] = myArray[j+1];
+                myArray[j+1] = temp;
+            }
+
+        }
+    }
+
+    //asending order
+    for(int i=0; i<SIZE; i++)
+    {
+        cout<< myArray[i] << " ";
+    }
+
+    cout <<endl;
+
+    //desending order
+    for(int i=SIZE-1; i>=0; i--)
+    {
+        cout<< myArray[i] << " ";
+    }
+
+    
+    return 0;
+}
