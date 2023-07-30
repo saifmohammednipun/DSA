0b96bcbe01a45c6f9f4569ead90e24e2bb2ff8a6
saifmohammednipun <saifmohammednipun@gmail.com>
2023-07-31 00:27:37 +0600

    Update Sorting

---
+++ Sorting/Insertion_Sort/1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+#define SIZE 5
+
+int main()
+{
+    int arr[SIZE] = {12, 11, 13, 5, 6};
+
+    for (int i = 1; i < SIZE; i++)
+    {
+        int current = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > current)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = current;
+    }
+
+    for (int k = 0; k < SIZE; k++)
+    {
+        cout << arr[k] << " ";
+    }
+
+    return 0;
+}
--- Sorting/Selection_Sort/1.cpp
+++ Sorting/Selection_Sort/1.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
 using namespace std;
+
+#define SIZE 4
 int main()
 {
+    int arr[SIZE] ={20,40,10,30};
+
+
+    //Asending Order
+    for(int i=0; i<SIZE-1; i++)
+    {
+        for(int j=i+1; j<SIZE; j++)
+        {
+            if(arr[i]>arr[j]) swap(arr[i],arr[j]); 
+        }
+    }
+
+    //Desending Order
+    // for(int i=0; i<SIZE-1; i++)
+    // {
+    //     for(int j=i+1; j<SIZE; j++)
+    //     {
+    //         if(arr[i]<arr[j]) swap(arr[i],arr[j]); 
+    //     }
+    // }
+
+    for(int i=0; i<SIZE; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
 
 
     return 0;
